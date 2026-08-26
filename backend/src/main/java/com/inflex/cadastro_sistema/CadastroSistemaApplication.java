882a7f32a56072090b941301ab89321a366f6324
Danillo O. Mochi <danillomochitls13@gmail.com>
2026-08-26 11:25:42 -0400

    project setup

---
+++ backend/src/main/java/com/inflex/cadastro_sistema/CadastroSistemaApplication.java
@@ -0,0 +1,13 @@
+package com.inflex.cadastro_sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroSistemaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/inflex/cadastro_sistema/CadastroSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflex.cadastro_sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
